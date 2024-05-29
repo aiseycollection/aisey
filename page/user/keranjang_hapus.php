f8d99ba0d3e678ae67b3e1deb8f8ac96d1995d24
aiseycollection <166523610+aiseycollection@users.noreply.github.com>
2024-05-29 14:36:06 +0700

    Add files via upload

---
+++ page/user/keranjang_hapus.php
@@ -0,0 +1,20 @@
+<?php
+include('lib/koneksi.php');
+
+		$id = $_GET['id'];
+// code by REGGIE INALAWIE
+		try {
+			$conn ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$pdo = $conn->prepare("DELETE FROM tbl_keranjang WHERE id = :id");
+			$deletedata = array(':id' => $id);
+			$pdo->execute($deletedata);
+// code by REGGIE INALAWIE
+      echo "<script>alert('Barang dalam keranjang berhasil dihapus');window.location='?page=beranda'</script>";
+// code by REGGIE INALAWIE
+		} catch (PDOexception $e) {
+			print "hapus berita gagal: " . $e->getMessage() . "<br/>";
+		   die();
+		}
+?>
+
+
